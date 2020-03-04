a104b45428ebe7b41b69dbf94c03eb85c774ae2e
wtphilbrook <wtphilbrook@plymouth.edu>
2020-03-04 16:02:26 -0500

    Lots of cleaning up, added date function and some extra information to the ending report.

--- Zoom_UserLicense_Project/scratch2.py
+++ Zoom_UserLicense_Project/scratch2.py
@@ -1,4 +1,6 @@
 import csv
+import time
+import datetime
 #
 # # # 
 # 
@@ -21,17 +23,26 @@ import csv
 #
 # # # 
 
+#Importing files
+user_file = 'zoomus_users.csv'
+meeting_data = 'meetinglistdetails_20200201_20200301.csv'
+
+
 # Grab the users and their licenses from a zoom output
-with open('zoomus_users.csv') as csvfile:
+with open(user_file) as csvfile:
     usersCSV = csv.reader(csvfile, delimiter=',')
     userslic = {}
+    liccount = 0
     for col in usersCSV:
         #Column 0 is email, Column 10 is license type
         userslic.update( {col[0] : col[10]} )
+        if col[10] == "Licensed":
+            liccount += 1
+
 
 # Grab a list of users and the duration of their zoom meeting.
 # Find any meeting longer than 45 minutes and create a dict with them and the user's email
-with open('meetinglistdetails_20200201_20200301.csv') as csvfile:
+with open(meeting_data) as csvfile:
     meetingCSV = csv.reader(csvfile, delimiter=',')
     meetings_over_45 = {}
     for col in meetingCSV:
@@ -53,13 +64,31 @@ with open('meetinglistdetails_20200201_20200301.csv') as csvfile:
 # not using 1 of the requirements for licensed users.
 #
 # Added another check for usersparts to see if they have meetings over 4 users or not
-print("Licensed Users who do NOT have meetings over 45 minutes or with more than 3 participants:")
+#
+# Added date/time to the top of the report
+# 
 print("")
+start_date = meeting_data.split('_')[1]
+end_date = meeting_data.split('_')[2].split('.')[0]
+fstart_date = datetime.datetime(int(start_date[0:4]),int(start_date[4:6]),int(start_date[6:])) 
+fend_date = datetime.datetime(int(end_date[0:4]),int(end_date[4:6]),int(end_date[6:]))
+print("Report covers dates between: ", fstart_date.strftime("%b-%d-%Y"),"to", fend_date.strftime("%b-%d-%Y"))
+print("")
+print("Licensed Users who do not have any meetings over 45 minutes or with more than 3 participants. \nAlso includes users who have not used Zoom during the range of this report:")
+print("")
+reclaimable = []
 for i in userslic.keys():
     if i not in meetings_over_45.keys():
         if userslic[i] == "Licensed":
             if i not in usersparts:
-                print(i)
+                reclaimable.append(i)
+                print(i.split('@')[0])
+
+#Some extra information for the report.
+print("")
+print("Total licenses used in this report: ", liccount)
+print("Number of licenses that can be reclaimed from this report: ",len(reclaimable))
+print("")
 
 
 
